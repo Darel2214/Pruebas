84a0c85c4504ee4ad07f7c006d6118d822b1e590
Alex Santacruz <alexdavid_95_@hotmail.com>
2023-12-08 19:57:28 -0500

    creacion del componente del listado de partidos

---
+++ client-socket/src/components/matches/index.jsx
@@ -0,0 +1,33 @@
+import { Link } from 'react-router-dom';
+import { ListGroup } from 'flowbite-react';
+import { IoFootball } from 'react-icons/io5';
+
+export const Matches = () => {
+  return (
+    <section className='flex justify-center items-center flex-col gap-2'>
+      <h3 className='dark:text-white text-lg italic'>Lista de Partidos</h3>
+
+      <ListGroup className='w-80'>
+        <Link to={'/partido/1'}>
+          <ListGroup.Item icon={IoFootball} active onClick={showDetails}>
+            Emelec VS Barcelona - Activo
+          </ListGroup.Item>
+        </Link>
+
+        <Link to={'/partido/2'}>
+          <ListGroup.Item icon={IoFootball}>Liga VS Barcelona - Finalizado</ListGroup.Item>
+        </Link>
+
+        <Link to={'/partido/3'}>
+          <ListGroup.Item icon={IoFootball}>Emelec VS Delfin - Finalizado</ListGroup.Item>
+        </Link>
+
+        <Link to={'/partido/4'}>
+          <ListGroup.Item icon={IoFootball} active>
+            Liga VS Delfin - Activo
+          </ListGroup.Item>
+        </Link>
+      </ListGroup>
+    </section>
+  );
+};
