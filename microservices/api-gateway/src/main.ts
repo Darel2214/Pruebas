a601669b818cd9e244224cc5ccd723a03e0f5168
Alex Santacruz <alexdavid_95_@hotmail.com>
2023-12-09 17:02:15 -0500

    habilitando cors en api gateway

--- microservices/api-gateway/src/main.ts
+++ microservices/api-gateway/src/main.ts
@@ -2,9 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionFilter } from './common/filters/http-exception.filter';
 import { TimeOutInterceptor } from './common/interceptors/timeout.interceptor';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
 
   app.useGlobalFilters(new AllExceptionFilter());
 
