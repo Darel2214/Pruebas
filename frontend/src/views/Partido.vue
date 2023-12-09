e3ee02f2088a4361342ee8b214766162e83216a8
Alex Santacruz <alexdavid_95_@hotmail.com>
2023-12-09 17:05:09 -0500

    creacion de logica del modulo de partidos

--- frontend/src/views/Partido.vue
+++ frontend/src/views/Partido.vue
@@ -1,148 +1,282 @@
 <template>
-  <div>
+  <div class="d-flex flex-column" style="gap: 10px">
     <h1 class="text-success">Partidos</h1>
-    <div class="mt-4">
-      <button @click="showCreateForm" class="btn btn-primary">
-        Agregar Jugador
-      </button>
+
+    <button @click="showCreateForm" class="btn btn-primary w-25">
+      Iniciar Partido
+    </button>
+
+    <!-- Formulario de creación -->
+    <div v-if="createForm">
+      <h2>Nuevo Partido</h2>
+
+      <form
+        @submit.prevent="create()"
+        class="d-flex flex-row align-items-center"
+        style="gap: 20px"
+      >
+        <select class="form-control" v-model="formData.equipo1" required>
+          <option :value="club.value" v-for="club in clubes" :key="club.value">
+            {{ club.name }}
+          </option>
+        </select>
+
+        <strong>VS</strong>
+
+        <select class="form-control" v-model="formData.equipo2" required>
+          <option :value="club.value" v-for="club in clubes" :key="club.value">
+            {{ club.name }}
+          </option>
+        </select>
+
+        <button type="submit" class="btn btn-success">Agregar</button>
+        <button @click="hideCreateForm" class="btn btn-danger">Cancelar</button>
+      </form>
+    </div>
+
+    <!-- Formulario de edición -->
+    <div v-if="editForm">
+      <h2>Actualizar Partido</h2>
+
+      <form
+        @submit.prevent="update()"
+        class="d-flex flex-row align-items-center"
+        style="gap: 20px"
+      >
+        <select class="form-control" v-model="editFormData.equipo1" required>
+          <option :value="club.value" v-for="club in clubes" :key="club.value">
+            {{ club.name }}
+          </option>
+        </select>
+
+        <strong>VS</strong>
+
+        <select class="form-control" v-model="editFormData.equipo2" required>
+          <option :value="club.value" v-for="club in clubes" :key="club.value">
+            {{ club.name }}
+          </option>
+        </select>
+
+        <input
+          v-model="editFormData.dateFormat"
+          type="date"
+          required
+          class="form-control"
+        />
+
+        <input
+          v-model="editFormData.hora"
+          type="time"
+          required
+          class="form-control"
+        />
+
+        <input
+          v-model="editFormData.marcador1"
+          type="number"
+          min="0"
+          required
+          class="form-control"
+        />
+
+        <input
+          v-model="editFormData.marcador2"
+          type="number"
+          min="0"
+          required
+          class="form-control"
+        />
+
+        <button type="submit" class="btn btn-success">Actualizar</button>
+        <button @click="hideEditForm" class="btn btn-danger">Cancelar</button>
+      </form>
     </div>
 
     <!-- Lista de jugadores en una tabla -->
-    <table class="table mt-4">
+    <table class="table table-striped">
       <thead class="thead-light">
         <tr>
-          <th>Nombre del Jugador</th>
-          <th>Dirección</th>
+          <th>Fecha de Partido</th>
+          <th>Equipo 1</th>
+          <th>Equipo 2</th>
+          <th>Marcador</th>
+          <th>Estado</th>
           <th>Acciones</th>
         </tr>
       </thead>
+
       <tbody>
         <tr
-          v-for="jugador in jugadores"
-          :key="jugador._id"
+          v-for="match in data"
+          :key="match._id"
           @mouseover="highlightRow($event)"
           @mouseleave="unhighlightRow($event)"
         >
-          <td>{{ jugador.nombre }}</td>
-          <td>{{ jugador.direccion }}</td>
+          <td>{{ match.dateFormat }} {{ match.hora }}</td>
+          <td>{{ match.equipo1 }}</td>
+          <td>{{ match.equipo2 }}</td>
+          <td>{{ match.marcador1 }} - {{ match.marcador2 }}</td>
+          <td>{{ match.estado }}</td>
           <td>
-            <button @click="editJugador(jugador)" class="btn btn-warning">
-              Editar
-            </button>
-            <button @click="deleteJugador(jugador._id)" class="btn btn-danger">
-              Eliminar
-            </button>
+            <div v-if="match.estado !== 'Activo'">
+              <button @click="showEditForm(match)" class="btn btn-warning mx-2">
+                Editar
+              </button>
+
+              <button @click="deleteMatch(match._id)" class="btn btn-danger">
+                Eliminar
+              </button>
+            </div>
           </td>
         </tr>
       </tbody>
     </table>
-
-    <!-- Formulario de creación/editar -->
-    <div v-if="showForm">
-      <h2>{{ isEditing ? 'Editar Jugador' : 'Agregar Jugador' }}</h2>
-      <form @submit.prevent="isEditing ? updateJugador() : createJugador()">
-        <input
-          v-model="formData.nombre"
-          placeholder="Nombre del jugador"
-          required
-          class="form-control"
-        />
-        <input
-          v-model="formData.direccion"
-          placeholder="Dirección del jugador"
-          required
-          class="form-control"
-        />
-        <button type="submit" class="btn btn-primary">
-          {{ isEditing ? 'Guardar Cambios' : 'Agregar' }}
-        </button>
-        <button @click="cancelEdit" class="btn btn-secondary">Cancelar</button>
-      </form>
-    </div>
   </div>
 </template>
 
 <script>
+import moment from 'moment';
+
 export default {
   data() {
     return {
-      jugadores: [],
-      showForm: false,
+      clubes: [],
+      data: [],
+      createForm: false,
       formData: {
-        nombre: '',
-        direccion: '',
+        equipo1: '',
+        equipo2: '',
       },
-      isEditing: false,
-      editingJugadorId: null,
-      highlightedRow: null,
+      editForm: false,
+      editFormData: {
+        equipo1: '',
+        equipo2: '',
+        marcador1: 0,
+        marcador2: 0,
+        fecha: null,
+        hora: '',
+      },
     };
   },
   methods: {
-    async fetchJugadores() {
+    async getClubes() {
       try {
-        const response = await this.$http.get('/jugador');
-        this.jugadores = response.data.jugador;
+        const { data } = await this.$http.get('/clubfutbol');
+
+        const clubes = data.clubFutbol.map(item => ({
+          name: item.nombreclub,
+          value: item.nombreclub,
+        }));
+
+        clubes.unshift({ name: 'Selecciona un equipo', value: '' });
+
+        this.clubes = clubes;
       } catch (error) {
-        console.error('Error al obtener los jugadores', error);
+        console.error('Error al obtener los partidos', error);
       }
     },
+
+    async fetchData() {
+      try {
+        const { data } = await this.$microservices.get('/match');
+
+        const matches = data.map(item => {
+          item.dateFormat = moment(item.fecha).format('YYYY-MM-DD');
+          return item;
+        });
+
+        this.data = matches;
+      } catch (error) {
+        console.error('Error al obtener los partidos', error);
+      }
+    },
+
     showCreateForm() {
-      this.showForm = true;
-      this.isEditing = false;
+      this.createForm = true;
       this.formData = {
-        nombre: '',
-        direccion: '',
+        equipo1: '',
+        equipo2: '',
       };
     },
-    async createJugador() {
-      try {
-        const response = await this.$http.post('/jugador', this.formData);
-        console.log(response.data.msg);
-        this.fetchJugadores();
-        this.showForm = false;
-      } catch (error) {
-        console.error('Error al agregar el jugador', error);
-      }
-    },
-    editJugador(jugador) {
-      this.showForm = true;
-      this.isEditing = true;
-      this.editingJugadorId = jugador._id;
+
+    hideCreateForm() {
+      this.createForm = false;
       this.formData = {
-        nombre: jugador.nombre,
-        direccion: jugador.direccion,
+        equipo1: '',
+        equipo2: '',
       };
     },
-    async updateJugador() {
+
+    async create() {
       try {
-        const response = await this.$http.put(
-          `/jugador/${this.editingJugadorId}`,
-          this.formData
-        );
-        console.log(response.data.msg);
-        this.fetchJugadores();
-        this.showForm = false;
+        const data = {
+          fecha: moment().toISOString(),
+          hora: moment().format('HH:mm'),
+          equipo1: this.formData.equipo1,
+          equipo2: this.formData.equipo2,
+          marcador1: 0,
+          marcador2: 0,
+          mesajes: [],
+          estado: 'Activo',
+        };
+
+        await this.$microservices.post('/match', data);
+        this.fetchData();
+        this.hideCreateForm();
       } catch (error) {
-        console.error('Error al actualizar el jugador', error);
+        console.error('Error al agregar partido', error);
       }
     },
-    async deleteJugador(jugadorId) {
-      try {
-        const response = await this.$http.delete(`/jugador/${jugadorId}`);
-        console.log(response.data.msg);
-        this.fetchJugadores();
-      } catch (error) {
-        console.error('Error al eliminar el jugador', error);
-      }
+
+    showEditForm(item) {
+      this.editForm = true;
+      this.editFormData = item;
     },
-    cancelEdit() {
-      this.showForm = false;
-      this.isEditing = false;
-      this.formData = {
-        nombre: '',
-        direccion: '',
+
+    hideEditForm() {
+      this.editForm = false;
+      this.editFormData = {
+        equipo1: '',
+        equipo2: '',
+        marcador1: 0,
+        marcador2: 0,
+        fecha: null,
+        hora: null,
       };
     },
+
+    async update() {
+      try {
+        const data = {
+          id: this.editFormData._id,
+          fecha: moment(
+            this.editFormData.dateFormat,
+            'YYYY-MM-DD'
+          ).toISOString(),
+          hora: this.editFormData.hora,
+          equipo1: this.editFormData.equipo1,
+          equipo2: this.editFormData.equipo2,
+          marcador1: Number(this.editFormData.marcador1),
+          marcador2: Number(this.editFormData.marcador2),
+        };
+
+        await this.$microservices.put(`/match/`, data);
+        this.fetchData();
+        this.hideEditForm();
+      } catch (error) {
+        console.error('Error al actualizar el partido', error);
+      }
+    },
+
+    async deleteMatch(id) {
+      try {
+        await this.$microservices.delete(`/match/${id}`);
+        this.fetchData();
+      } catch (error) {
+        console.error('Error al eliminar el partido', error);
+      }
+    },
+
     highlightRow(event) {
       event.currentTarget.style.backgroundColor = '#cceeff';
     },
@@ -151,11 +285,8 @@ export default {
     },
   },
   mounted() {
-    this.fetchJugadores();
+    this.fetchData();
+    this.getClubes();
   },
 };
 </script>
-
-<style scoped>
-/* Estilos personalizados o de Bootstrap si es necesario */
-</style>
