bcd46b85028662147403a1b0d01e40a794f13654
Alex Santacruz <alexdavid_95_@hotmail.com>
2023-12-10 00:55:39 -0500

    integrando api de microservicios para obtener el listado de partidos

--- client-socket/src/components/matches/index.jsx
+++ client-socket/src/components/matches/index.jsx
@@ -1,32 +1,38 @@
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { ListGroup } from 'flowbite-react';
+import axios from 'axios';
+
 import { IoFootball } from 'react-icons/io5';
 
 export const Matches = () => {
+  const [data, setData] = useState([]);
+
+  const getData = async () => {
+    try {
+      const { data } = await axios.get(import.meta.env.VITE_API + '/api/v2/match');
+      setData(data ?? []);
+    } catch (error) {
+      console.log('error', error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <section className='flex justify-center items-center flex-col gap-2'>
       <h3 className='dark:text-white text-lg italic'>Lista de Partidos</h3>
 
       <ListGroup className='w-80'>
-        <Link to={'/partido/1'}>
-          <ListGroup.Item icon={IoFootball} active>
-            Emelec VS Barcelona - Activo
-          </ListGroup.Item>
-        </Link>
-
-        <Link to={'/partido/2'}>
-          <ListGroup.Item icon={IoFootball}>Liga VS Barcelona - Finalizado</ListGroup.Item>
-        </Link>
-
-        <Link to={'/partido/3'}>
-          <ListGroup.Item icon={IoFootball}>Emelec VS Delfin - Finalizado</ListGroup.Item>
-        </Link>
-
-        <Link to={'/partido/4'}>
-          <ListGroup.Item icon={IoFootball} active>
-            Liga VS Delfin - Activo
-          </ListGroup.Item>
-        </Link>
+        {data.map((item, index) => (
+          <Link to={'/partido/' + item._id} key={index}>
+            <ListGroup.Item icon={IoFootball} active={item.estado !== 'Activo'}>
+              {item.equipo1} VS {item.equipo2} - {item.estado}
+            </ListGroup.Item>
+          </Link>
+        ))}
       </ListGroup>
     </section>
   );
