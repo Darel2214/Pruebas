06f0753488ac65b2b87a982a1600c62f8a6e3f1c
Alex Santacruz <alexdavid_95_@hotmail.com>
2023-12-10 00:56:17 -0500

    integracion de websocket para ver la actualizacion de partidos en tiempo real

--- client-socket/src/components/match/index.jsx
+++ client-socket/src/components/match/index.jsx
@@ -1,18 +1,42 @@
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect, Fragment } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { io } from 'socket.io-client';
 import { Button } from 'flowbite-react';
 
 export const Match = () => {
+  const { id } = useParams();
+
+  const [match, setMatch] = useState(null);
+
+  const initData = async () => {
+    const socket = io(import.meta.env.VITE_API);
+
+    socket.emit('get-match', id);
+
+    socket.on('match', (payload) => setMatch(payload));
+  };
+
+  useEffect(() => {
+    initData();
+  }, []);
+
   return (
     <section className='flex justify-center items-center flex-col gap-5'>
-      <h3 className='dark:text-white text-lg italic'>Emelec Vs Barcelona</h3>
+      {match && (
+        <Fragment>
+          <h3 className='dark:text-white text-lg italic'>
+            {match?.equipo1} Vs {match?.equipo2}
+          </h3>
 
-      <article className='flex flex-row gap-10 justify-between items-center dark:text-white text-5xl'>
-        <strong>5</strong>
-        <p>-</p>
-        <strong>1</strong>
-      </article>
+          <article className='flex flex-row gap-10 justify-between items-center dark:text-white text-5xl'>
+            <strong>{match?.marcador1}</strong>
+            <p>-</p>
+            <strong>{match?.marcador2}</strong>
+          </article>
 
-      <p className='dark:text-white italic'>Falta de Jugador Pepito</p>
+          <p className='dark:text-white italic'>{match?.mensajes[match.mensajes.length - 1]}</p>
+        </Fragment>
+      )}
 
       <Link to={'/'}>
         <Button gradientDuoTone='pinkToOrange'>Regresar</Button>
